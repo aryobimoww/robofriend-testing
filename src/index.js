60bc9146bafb227a414ede71871d8a7742b4f551
aryobimoww <aryobimoww19@gmail.com>
2021-08-24 09:51:06 +0700

    pwa

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import reportWebVitals from './reportWebVitals';
 import App from './Containers/App';
 import 'tachyons';
 import { searchRobots, requestRobots } from './reducer';
+import RegisterServiceworker from './registerServiceWorker;' 
 
 const logger = createLogger();
 const rootReducer = combineReducers({searchRobots,requestRobots})
@@ -19,7 +20,7 @@ ReactDOM.render(
 <App/>
 </Provider>,document.getElementById('root')
 );
-
+RegisterServiceworker();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
